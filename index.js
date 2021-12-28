b5295d9e4ad3de4d5926c07354fc35e1a8bca60e
tashana1999 <85959669+tashana1999@users.noreply.github.com>
2021-12-28 17:39:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//document.getElementById("count-el").innerText=5
+let saveEl=document.getElementById("save-el")
+let countEl=document.getElementById("count-el")
+//console.log(countEl)
+let count=0
+function increment(){
+   // console.log("clicked")
+    count=count+1
+    countEl.textContent=count
+   // console.log(count)
+}
+function save(){
+    let countStr=count +"-"
+    saveEl.textContent=countStr
+    countEl.textContent=0
+    count=0
+}
